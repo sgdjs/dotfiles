ac028b365f1cce6ba7b9f7c612353d57e9db732a
Simon <simon.gdjs@gmail.com>
2017-01-15 14:08:39 -0500

    Add .dotfiles-reset.sh

---
+++ .dotfiles-reset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf ~/.dotfiles
+rm -rf ~/.vim
+rm -rf ~/.tmux
+rm ~/.bash_profile
+rm ~/.git-completion.bash
+rm ~/.gitignore
+rm ~/.gitmodules
+rm ~/.tmux.conf
+rm ~/README.md
+git clone --bare https://github.com/sgdjs/dotfiles.git $HOME/.dotfiles
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME/ config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME/ checkout vundle
